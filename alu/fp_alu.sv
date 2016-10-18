4d82122bdc2cbcaef80a5a74a0a264ae494c8a76
Jai Rajgarhia <jrajgarh@ece005.campus.ece.cmu.local>
2016-10-18 16:36:29 -0400

    cleaned it up a bit

--- alu/fp_alu.sv
+++ alu/fp_alu.sv
@@ -1,3 +1,10 @@
+`default_nettype none 
+
+`define ADD 2'b00
+`define SUB 2'b01
+`define MUL 2'b10
+`define DIV 2'b11
+
 module fp_alu
   #(parameter n = 32)
    (input logic [n-1:0] a, b,
@@ -11,17 +18,20 @@ module fp_alu
    
    always_comb begin
       buf_result = 0;
+      result = 0;
+      buf_a = 0;
+      buf_b = 0;
       case(op)
-	ADD: result = a + b;
-	SUB: result = a + (~b+1);
-	MUL: begin
+	`ADD: result = a + b;
+	`SUB: result = a + (~b+1);
+	`MUL: begin
 	   buf_result = {s,a[n-2:0]*b[n-2:0]};
 	   buf_result = buf_result >> (n/2);
 	   result = buf_result[n-1:0];
 	end
-	DIV: begin
-	   buf_a = {(n+1)'d0,a[n-2:0]};
-	   buf_b = {(n+1)'d0,b[n-2:0]};
+	`DIV: begin
+	   buf_a = {{(n+1){1'b0}},a[n-2:0]};
+	   buf_b = {{(n+1){1'b0}},b[n-2:0]};
 	   buf_a = buf_a << (n/2);
 	   buf_result = buf_a/buf_b;
 	   result = buf_result[n-2:0];
@@ -31,4 +41,22 @@ module fp_alu
    end // always_comb
 
 endmodule: fp_alu
+/*
+module tb
+  (output logic [31:0] a, b,
+   output logic [1:0]  op);
 
+   logic [31:0]        result;
+   
+   fp_alu dut(a, b, op, result);
+   
+   initial begin
+      a = 'd3;
+      b = 'd2;
+      op = `MUL;
+      
+     #10  $display("result=%d",result);
+   end
+
+endmodule: tb
+*/
